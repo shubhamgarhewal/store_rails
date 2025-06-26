bf260a68a9912b73f28dd91ef4f9cc398bbdafb4
shubhamgarhewal <shubhamgarhewal2@gmail.com>
2025-06-26 12:53:39 +0530

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,11 @@ Rails.application.routes.draw do
   get "/products", to: "products#index"
   root "products#index"
 
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+  resource :unsubscribe, only: [ :show ]
+
   resource :session
   resources :passwords, param: :token
 end
